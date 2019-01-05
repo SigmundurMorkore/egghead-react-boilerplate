c9023f3f2080193f06e9f97008dc612ee66805c7
sisomagmomunden <sisomagmomunden@gmail.com>
2019-01-05 04:50:09 +0000

    Added proptypes for DefaultErrorBoundary.js

--- src/DefaultErrorBoundary.js
+++ src/DefaultErrorBoundary.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import propTypes from 'prop-types'
 
 export default class DefaultErrorBoundary extends React.Component {
   state = {
@@ -9,9 +10,13 @@ export default class DefaultErrorBoundary extends React.Component {
     return { isError: true }
   }
 
+  static propTypes = {
+    children: propTypes.node.isRequired
+  }
+
   render() {
     const { isError } = this.state
-    const { children } = this.props // eslint-disable-line
+    const { children } = this.props
 
     return isError ? <div>Something went wrong!</div> : children
   }
